507d4164f2d143243cdbe2561fb3a27be4680ad5
Kurt Rank <kurtrank@gmail.com>
2021-10-15 13:24:11 -0400

    feat: support plugins and `config.wpRelease` options in package.json
    This enables this module to be used for a wider variety of plugins and
    themes. Now any project using this can easily add a custom ignore list
    and entry file to update the version number in that WordPress is
    concerned with. Additionally, the zip functionality has been moved into
    node rather than spawning a subprocess to run `zip`.

--- release-prep.js
+++ release-prep.js
@@ -4,7 +4,8 @@
  * External Dependencies
  */
 const replace = require("replace-in-file");
-const { writeFileSync } = require("fs");
+const { writeFileSync, existsSync, createWriteStream } = require("fs");
+const archiver = require("archiver");
 const spawn = require("cross-spawn");
 
 /**
@@ -23,25 +24,36 @@ if (!args[0]) {
 	const lastRelease = args[1] ? args[0] : false;
 	const nextRelease = args[1] ? args[1] : args[0];
 
-	// update style.css
-	const options = {
-		files: "style.css",
-		from: /Version: (.*)/g,
-		to: `Version: ${nextRelease}`,
-	};
+	const package = require(`${projectRoot}/package.json`);
+	const packageLock = require(`${projectRoot}/package-lock.json`);
 
-	try {
-		const results = replace.sync(options);
-	} catch (error) {
-		console.error("Error replacing version in style.css:", error);
+	const wpFile = package.config?.wpRelease?.entryFile
+		? package.config?.wpRelease?.entryFile
+		: existsSync(`${projectRoot}/style.css`)
+		? "style.css"
+		: existsSync(`${projectRoot}/${projectName}.php`)
+		? `${projectName}.php`
+		: false;
+
+	if (wpFile) {
+		const options = {
+			files: wpFile,
+			from: /Version: (.*)/g,
+			to: `Version: ${nextRelease}`,
+		};
+
+		try {
+			const results = replace.sync(options);
+		} catch (error) {
+			console.error("Error replacing version in style.css:", error);
+		}
+	} else {
+		console.log(
+			'Could not find a file to update with new version. If you\'re using something other than `style.css` or `{plugin-name}.php` to house the "Version: x.y" header WordPress uses, please specify in package.json under `config.wpRelease.entryFile`. Example:\n\n{\n\t"config": {\n\t\t"wpRelease": {\n\t\t\t"entryFile: "custom-filename.php"\n\t\t}\n\t}\n}'
+		);
 	}
 
 	// update package files
-	const packageFile = `${projectRoot}/package.json`;
-	const packageLockFile = `${projectRoot}/package-lock.json`;
-	const package = require(packageFile);
-	const packageLock = require(packageLockFile);
-
 	package.version = nextRelease;
 	packageLock.version = nextRelease;
 
@@ -56,17 +68,46 @@ if (!args[0]) {
 		}
 	);
 
-	// build zip file
-	spawn.sync(
-		"zip",
-		[
-			"-r",
-			`../${projectName}.zip`,
-			".",
-			`-x@${projectRoot}/node_modules/wp-release/.zip-exclude.lst`,
-		],
-		{
-			stdio: "inherit",
+	const output = createWriteStream(`${projectRoot}/../${projectName}.zip`);
+	const zip = archiver("zip", {
+		zlib: { level: 9 },
+	});
+
+	output.on("close", function () {
+		console.log(`zip file created: ${archive.pointer()} total bytes`);
+	});
+
+	zip.on("warning", function (err) {
+		if (err.code === "ENOENT") {
+			// log warning
+		} else {
+			// throw error
+			throw err;
 		}
+	});
+
+	zip.pipe(output);
+
+	ignoreList = package.config?.wpRelease?.ignore ?? [
+		".DS_Store",
+		".env",
+		".env.sample",
+		".git/*",
+		".github/*",
+		".gitignore",
+		".huskyrc.js",
+		".releaserc",
+		"node_modules/*",
+		"release.config.js",
+	];
+
+	zip.glob(
+		"**/*",
+		{
+			cwd: projectRoot,
+			ignore: ignoreList,
+		},
+		{}
 	);
+	zip.finalize();
 }
